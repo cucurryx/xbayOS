9c89291191a07547afa6a2a27c7dc71fd34e1010
xiebei1108 <xiebei1108@outlook.com>
2018-10-24 17:45:25 +0800

    finish put_str

--- lib/print.h
+++ lib/print.h
@@ -3,6 +3,10 @@
 
 #include "stdint.h"
 
+// character print function, just like putchar in C.
 void put_char(uint8_t c);
+
+// string print function, just like puts in C.
+void put_str(char* s);
 
 #endif
