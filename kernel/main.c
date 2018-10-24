09726293c305537588518e317a53a7857b24f6f0
xiebei1108 <xiebei1108@outlook.com>
2018-10-24 16:59:30 +0800

    put_char test ok

---
+++ kernel/main.c
@@ -0,0 +1,19 @@
+#include "print.h"
+
+int main() {
+
+  put_char('h');
+  put_char('e');
+  put_char('l');
+  put_char('l');
+  put_char('o');
+
+  put_char('w');
+  put_char('o');
+  put_char('r');
+  put_char('l');
+  put_char('d');
+  
+  while (1);
+  return 0;
+}
