06482962ba7bb1cf0e66ab16a61b077d9a84ca40
xiebei1108 <xiebei1108@outlook.com>
2018-11-11 15:21:39 +0800

    fix bug: thread schedule

--- kernel/main.c
+++ kernel/main.c
@@ -13,14 +13,17 @@ void *start(void *arg);
 int main() {
     put_str("I am kernel\n");
     init_all();
-    intr_enable();
-
-    while (true) {
-        put_str("main ");
-    }
-
     thread_start("thread1", 16, start, (void*)"thread1 ");
     thread_start("thread2", 16, start, (void*)"thread2 ");
+
+    intr_enable();
+
+    put_str("interrupt enable!\n");
+    while (true) {
+        intr_enable();
+        put_str("main ");
+        intr_disable();
+    }
 
     while (1);
 }
@@ -29,7 +32,8 @@ int main() {
 void *start(void *arg) {
     char *s = (char*)arg;
     while (true) {
+        intr_enable();
         put_str(s);
-        put_char('\n');
+        intr_disable();
     }
 }
