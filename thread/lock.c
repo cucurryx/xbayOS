9dac51eb6657a1b0c1bb0609d86390af42f10409
xiebei1108 <xiebei1108@outlook.com>
2018-11-13 23:51:42 +0800

    finish mutex

--- thread/lock.c
+++ thread/lock.c
@@ -3,11 +3,14 @@
 #include "thread.h"
 #include "debug.h"
 
+//初始化信号量
 void sem_init(sem_t *sem, uint8_t value) {
     sem->value = value;
     list_init(&sem->waiters);
 }
 
+//递增信号量，需要关闭中断，来满足互斥
+//然后根据情况来更新waiter队列，如果有阻塞线程，那么解除其阻塞状态
 void sem_up(sem_t *sem) {
     intr_stat state;
     INTERRUPT_DISABLE(state);
@@ -23,6 +26,8 @@ void sem_up(sem_t *sem) {
     INTERRUPT_RESTORE(state);
 }
 
+//递减互斥量，需要关闭中断，满足互斥
+//如果信号量减为0，则阻塞调用该函数的线程，并加入waiter队列
 void sem_down(sem_t *sem) {
     intr_stat state;
     INTERRUPT_DISABLE(state);
@@ -40,14 +45,36 @@ void sem_down(sem_t *sem) {
     INTERRUPT_RESTORE(state);
 }
 
+//mutex就是一个二元信号量
 void mutex_init(mutex_t *mutex) {
-
+    mutex->holder = NULL;
+    mutex->recursion = 0;
+    sem_init(&mutex->sem, 1);
 }
 
+//互斥锁加锁，判断是否为重复加锁，如果重复加锁，那么只需要递增递归数
+//否则，递减信号量，并更新holder和recursion
 void mutex_lock(mutex_t *mutex) {
-
+    if (mutex->holder != running_thread()) {
+        sem_down(&mutex->sem);
+        ASSERT(mutex->recursion == 0);
+        mutex->holder = running_thread();
+        ++mutex->recursion;
+    } else {
+        ++mutex->recursion;
+    }
 }
 
+//互斥锁解锁，判断是否需要让出该锁
+//如果需要让出，那么更新holder和recursion，递增信号量
+//否则，只需要递减递归数
 void mutex_unlock(mutex_t *mutex) {
-
+    ASSERT(mutex->holder = running_thread());
+    if (mutex->recursion == 1) {
+        mutex->holder = NULL;
+        mutex->recursion = 0;
+        sem_up(&mutex->sem);
+    } else {
+        --mutex->recursion;
+    }
 }
--- thread/lock.h
+++ thread/lock.h
@@ -9,16 +9,15 @@ typedef struct __mutex_t mutex_t;
 typedef struct __sem_t sem_t;
 
 struct __sem_t {
-    uint8_t value;
-    list waiters;
+    uint8_t value;          //信号量的值
+    list waiters;           //阻塞在该信号量上的线程
 };
-
 
 struct __mutex_t {
-    sem_t sem;
-    task_struct *holder;
+    sem_t sem;              //信号量
+    task_struct *holder;    //该互斥锁的持有线程
+    uint8_t recursion;      //递归层数，用于防止递归加锁
 };
-
 
 void sem_init(sem_t *sem, uint8_t value);
 void sem_up(sem_t *sem);
